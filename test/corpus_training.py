018d87221c01dce1133df3ff7119a98ba543e856
José Manuel <jmgomez.soriano@gmail.com>
2021-03-30 21:41:14 +0200

    Training a simple model.

--- test/corpus_training.py
+++ test/corpus_training.py
@@ -3,12 +3,14 @@ import unittest
 from scipy.sparse import lil_matrix
 
 from corptrans import Corpus
+from corptrans.corpus.corpus import ArrayCorpus
 from corptrans.corpus.csv import CsvCorpus
 from corptrans.transformers import ClassReduction
 from corptrans.transformers.csv import CsvTransformer
 from corptrans.transformers.embeddings import EmbeddingExtension
 from corptrans.transformers.gensim import Tokens2Freq, Dict2Tuples, BoW, TfIdf
 from corptrans.transformers.preprocess import Preprocess
+from eval import metrics, print_metrics
 from sklearn_train import create_ml
 
 
@@ -22,7 +24,7 @@ def corpus2matrix(corpus: Corpus) -> lil_matrix:
 
 class MyTestCase(unittest.TestCase):
     def test_trainning(self):
-        with CsvCorpus('data/reddit_corpus_agree.csv') as corpus:
+        with CsvCorpus('data/gold_reddit_corpus_agree.csv') as corpus:
             corpus.add_transformer(CsvTransformer('text', 'cls'))
             corpus.add_transformer(ClassReduction({0: ['No risk'], 1: ['Risk']}))
             corpus.add_transformer(Preprocess())
@@ -33,12 +35,22 @@ class MyTestCase(unittest.TestCase):
             corpus.add_transformer(TfIdf())
             train_corpus = corpus.train()
         ml = create_ml(corpus2matrix(train_corpus), train_corpus.classes(), 'SVM')
-        with CsvCorpus('data/gold_reddit_corpus_agree.csv') as test_corpus:
-            # corpus_test.add_transformer(corpus_train.transformers)
+        with CsvCorpus('data/reddit_corpus_agree.csv') as test_corpus:
             test_corpus.set_transformers(train_corpus.transformers)
+            test_corpus = ArrayCorpus(test_corpus)
+            test_corpus._metadata = corpus.metadata
+            X = corpus2matrix(test_corpus)
+            y = test_corpus.classes()
+        with CsvCorpus('data/reddit_corpus_agree.csv') as test_corpus:
+            test_corpus.add_transformer(CsvTransformer('text', 'cls'))
+            test_corpus.add_transformer(ClassReduction({0: ['No risk'], 1: ['Risk']}))
+            test_corpus = ArrayCorpus(test_corpus)
 
-            for sample in test_corpus:
-                print(sample)
+            y_pred = [ml.predict(X[i])[0] for i in range(X.shape[0])]
+            print_metrics(metrics(y, y_pred))
+            for i, y1 in enumerate(y):
+                print(test_corpus[i], y_pred[i], sep=': ')
+
         self.assertEqual(True, True)
 
 
