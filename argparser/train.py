91b18cf08f92d36d349cb2c74543d86fb21d46c1
José Manuel <jmgomez.soriano@gmail.com>
2021-04-20 20:50:23 +0200

    Refactoring.

--- argparser/train.py
+++ argparser/train.py
@@ -1,8 +1,13 @@
 from argparse import ArgumentParser
 
-RASA = 'rasa'
-SKLEARN = 'sklearn'
+RASA, SKLEARN = 'rasa', 'sklearn'
 ENGINES = [RASA, SKLEARN]
+SVM, KMEANS = 'svm', 'kmeans'
+CLASSIFIERS = [SVM, KMEANS]
+CLASS_ENGINES = {
+    RASA: {},
+    SKLEARN: {c for c in CLASSIFIERS}
+}
 DEF_TEXT_COLUMN = 'text'
 DEF_CLASS_COLUMN = 'cls'
 
@@ -21,6 +26,10 @@ class TrainArgParser(object):
         return self._args.engine
 
     @property
+    def classifier(self) -> str:
+        return self._args.classifier
+
+    @property
     def tfidf(self) -> bool:
         return self._args.tfidf
 
@@ -29,8 +38,20 @@ class TrainArgParser(object):
         return  self._args.lsi
 
     @property
-    def embedding(self) -> bool:
-        return self._args.embedding
+    def lda(self) -> bool:
+        return  self._args.lsi
+
+    @property
+    def embeddings(self) -> bool:
+        return self._args.embeddinga
+
+    @property
+    def embedding_num(self) -> int:
+        return self._args.embedding_num
+
+    @property
+    def embedding_threshold(self) -> int:
+        return self._args.embedding_threshold
 
     @property
     def value_column(self) -> str:
@@ -40,11 +61,14 @@ class TrainArgParser(object):
     def class_column(self) -> str:
         return self._args.class_column
 
+    @property
+    def output(self) -> str:
+        return self._args.output
+
     def __init__(self) -> None:
-        parser = ArgumentParser(description='Train a model.')
-        self.set_arguments(parser)
-        self._args = parser.parse_args()
-        self._args = parser.parse_args()
+        self._parser = ArgumentParser(description='Train a model.')
+        self.set_arguments(self._parser)
+        self._args = self._parser.parse_args()
 
     @staticmethod
     def set_arguments(parser: ArgumentParser) -> None:
@@ -58,6 +82,9 @@ class TrainArgParser(object):
         parser.add_argument('-e', '--engine', type=str, metavar='ENGINE', default=SKLEARN,
                             help=f'The machine learning engine to use to train. Available engines: {ENGINES}. '
                                  f'By default, {SKLEARN}')
+        parser.add_argument('--classifier', type=str, metavar='CLASSIFIER', default=SVM,
+                            help=f'The suitable classifier to use for the specific engine to use to train. '
+                                 f'Available classifiers: {CLASSIFIERS}. By default, {SVM}')
         parser.add_argument('--tfidf', action='store_true', default=False,
                             help=f'Use TF/Idf for feature weighted. Only for {SKLEARN} engine.')
         parser.add_argument('--lsi', action='store_true', default=False,
@@ -66,5 +93,10 @@ class TrainArgParser(object):
                             help=f'Use LDA features. Only for {SKLEARN} engine.')
         parser.add_argument('--embeddings', action='store_true', default=False,
                             help=f'Use word embeddings for text expansions. Only for {SKLEARN} engine.')
-        parser.add_argument('--test', metavar='FILE', type=str,
-                            help=f'The corpus used to test the model.')
+        parser.add_argument('--embedding_num', metavar='NUM', type=int, default=10,
+                            help=f'If the embeddings are used, the number of embeddings. By default, 10.')
+        parser.add_argument('--embedding_threshold', metavar='VALUE', type=int, default=0.85,
+                            help=f'If embeddings are used, the threshold of accepted embeddings. By default, 0.85.')
+        parser.add_argument('--output', metavar='FILE', type=str,
+                            help=f'The file with the trained model. For sklearn engine it would be a pickle file and '
+                                 f'for rasa it would be a tar.gz file.')
--- train.py
+++ train.py
@@ -1,9 +1,88 @@
-from argparser.train import TrainArgParser
+from abc import abstractmethod, ABCMeta, ABC
+from typing import Any
+
+from argparser.train import TrainArgParser, SKLEARN, RASA, ENGINES
+from corptrans import Transformers
+from corptrans.corpus.csv import CsvCorpus
+from corptrans.transformers import ClassReduction
+from corptrans.transformers.csv import CsvTransformer
+from corptrans.transformers.embeddings import EmbeddingExtension
+from corptrans.transformers.gensim import Tokens2Freq, Dict2Tuples, BoW, TfIdf, Lsi, Lda
+from corptrans.transformers.preprocess import Preprocess
+
+
+class Trainer(ABC):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def train(self, corpus: str):
+        pass
+
+    @abstractmethod
+    def save(self, file: str):
+        pass
+
+
+class StandardNlpTransformer(Transformers):
+    def __init__(self, lang: str, tfidf: bool, lsi: bool, lda: bool,
+                 embeddings: bool, embedding_num: int, embedding_threshold: float) -> None:
+        super(StandardNlpTransformer, self).__init__()
+        self.add(Preprocess())
+        self.add(Tokens2Freq())
+        self.add(EmbeddingExtension(lang, embedding_num, embedding_threshold) if embeddings else None)
+        self.add(Dict2Tuples())
+        self.add(BoW())
+        self.add(TfIdf() if tfidf else None)
+        self.add(Lsi() if lsi else None)
+        self.add(Lda() if lda else None)
+
+
+class SkLearnTrainer(Trainer):
+    def __init__(self, classifier: str, lang: str, tfidf: bool, lsi: bool, lda: bool,
+                 embeddings: bool, embedding_num: int, embedding_threshold: float):
+        self.__classifier = classifier
+        self.__lang = lang
+        self.__tfidf = tfidf
+        self.__lsi = lsi
+        self.__lda = lda
+        self.__embeddings = embeddings
+        self.__embedding_num = embedding_num
+        self.__embedding_threshold = embedding_threshold
+
+    def train(self, fname: str) -> Any:
+        with CsvCorpus(fname) as corpus:
+            corpus.add_transformer(CsvTransformer('text', 'cls'))
+            corpus.add_transformer(ClassReduction({0: ['No risk'], 1: ['Possible', 'Risk', 'Urgent', 'Immediate']}))
+            corpus.add_transformer(StandardNlpTransformer(self.__lang, self.__tfidf, self.__lsi, self.__lda,
+                                                          self.__embeddings, self.__embedding_num,
+                                                          self.__embedding_threshold))
+            train_corpus = corpus.train()
+
+
+    def save(self, fname: str):
+        pass
+
+
+class RasaTrainer(Trainer):
+    pass
+
+
+def build_trainer(engine: str, classifier: str, **kwargs) -> Trainer:
+    if engine == SKLEARN:
+        return SkLearnTrainer(classifier, kwargs['lang'], kwargs['tfidf'], kwargs['lsi'], kwargs['lda'],
+                              kwargs['embeddings'], kwargs['embedding_num'], kwargs['embedding_threshold'])
+    if engine == RASA:
+        return RasaTrainer()
+    raise ValueError(f'The {engine} is not a valid engine. The available engines are: {ENGINES}')
 
 
 def main() -> None:
     args = TrainArgParser()
-
+    trainer = build_trainer(args.engine, args.classifier, tfidf=args.tfidf, lsi=args.lsi, lda=args.lda,
+                            embeddings=args.embeddings, embedding_num=args.embedding_num,
+                            embedding_threshold=args.embedding_threshold)
+    trainer.train(args.train)
+    trainer.save(args.output)
 
 
 if __name__ == '__main__':
